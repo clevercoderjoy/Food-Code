ddb76bd6e68e7bf86ea943d6bf861cb0d9a0ec5a
clevercoderjoy <clevercoderjoy@gmail.com>
2023-12-17 00:37:11 +0530

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,6 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const res_url =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9335741&lng=77.6081917&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9335741&lng=77.6081917&is-seo-homepage-enabled=true";
+
+  // https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.931842&lng=77.60850270000002&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
