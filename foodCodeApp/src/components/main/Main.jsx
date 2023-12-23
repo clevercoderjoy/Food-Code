46c5b311504a047ce691496f6a93b589dabf1121
clevercoderjoy <clevercoderjoy@gmail.com>
2023-12-23 23:58:21 +0530

    feat: implemented auth buttons

--- foodCodeApp/src/components/main/Main.jsx
+++ foodCodeApp/src/components/main/Main.jsx
@@ -14,8 +14,8 @@ const Main = () => {
     {
       const response = await fetch(res_url);
       const data = await response.json()
-      console.log(data.data)
-      const apiResData = (await data)?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      console.log(data.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+      const apiResData = (await data)?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
       setResData(apiResData);
       setFilteredRestaurants(apiResData);
     }
