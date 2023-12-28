4d92c0e913f58dccda4b6bf9d792c11dcd51ffd0
clevercoderjoy <clevercoderjoy@gmail.com>
2023-12-28 05:45:19 +0530

    feat: implemented false route page

---
+++ src/pages/error/Error.jsx
@@ -0,0 +1,17 @@
+import "./error.css";
+
+const Error = () => {
+  return (
+    <>
+      <div className="errorContainer">
+        <h1>
+          <span>OOPs!!!</span>
+          <span>Something Went Wrong</span>
+        </h1>
+        <h2>Ask <a href="https://clevercoderjoy.bio.link/" target="_blank" rel="noopener noreferrer">clevercoderjoy</a> To Fix It</h2>
+      </div>
+    </>
+  );
+}
+
+export default Error;
