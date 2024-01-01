fbe1aea274169089229832266810ade0958725b0
clevercoderjoy <clevercoderjoy@gmail.com>
2024-01-02 05:00:17 +0530

    extracted food from data

--- src/components/foodCard/FoodCard.jsx
+++ src/components/foodCard/FoodCard.jsx
@@ -1,8 +1,14 @@
 import "./foodCard.css";
 
 const FoodCard = ({ foodItems }) => {
-  console.log(foodItems);
+  // console.log(foodItems);
   console.log(foodItems?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+  const menuItems = foodItems?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+  const display = () => {
+    const x = foodItems?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+    x.map((xx, index) => { index !== 0 && index !== 7 && index !== 8 ? (console.log(xx.card.card.title)) : "" })
+  }
+  // display();
   return (
     <>
       <div className="menuContainer">
@@ -41,6 +47,36 @@ const FoodCard = ({ foodItems }) => {
               foodItems?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle.offers.map((offer) => <div className="offerCarousel" key={offer?.resId}>{offer?.info?.header}</div>)
             }
           </div>
+        </div>
+        <div className="menuItems">
+          {
+            menuItems.map((items, index) => {
+              return (index !== 0 && index !== 7 && index !== 8) ? (
+                <div className="categorizedItems" key={index}>
+                  <div className="categoryTitle">{items?.card?.card?.title}</div>
+                  <div className="categoryItems">
+                    {
+                      items?.card?.card?.itemCards.map((item) => {
+                        const { name, description, imageId, isVeg, price, id } = item.card.info;
+                        return (
+                          <div className="foodItem" key={id}>
+                            <div className="foodDetails">
+                              <span className="foodIsVeg">{isVeg}</span>
+                              <span className="foodName">{name}</span>
+                              <span className="foodDescription">{description}</span>
+                              <span className="foodPrice">{price}</span>
+                            </div>
+                            <br />
+                            <div className="foodImg">imageId {imageId}</div>
+                          </div>
+                        );
+                      })
+                    }
+                  </div>
+                </div>
+              ) : null;
+            })
+          }
         </div>
       </div>
     </>
