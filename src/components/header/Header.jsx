705d462972b4ed1eddf9dd3bc97a22cd00799bf3
clevercoderjoy <clevercoderjoy@gmail.com>
2024-01-19 07:53:22 +0530

    fix: fixed un-aligned header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   const authButtonClass = "loginButton text-center block border-none text-white font-bold py-[0.1rem] px-[0.2rem] transition-all duration-[100] ease-in-out rounded-[3px] uppercase tracking-widest";
 
-  const listStyle = "inline-block text-xl p-2 cursor-pointer transition-all duration-110 ease-in-out hover:font-bold hover:scale-[1.1]";
+  const listStyle = "inline-block text-xl p-2 cursor-pointer transition-all duration-110 ease-in-out hover:font-bold hover:scale-[1.1] m-auto";
 
   const [btnState, setBtnState] = useState({ class: `bg-[#0f9d58] ${authButtonClass}`, btnTxt: "Login" });
 
@@ -23,10 +23,10 @@ const Header = () => {
 
   return (
     <>
-      <header className="headerContainer relative flex items-center justify-between m-[0.3rem] border-2 border-black rounded-[3px]">
+      <header className="headerContainer flex items-center justify-between m-[0.3rem] border-2 border-black rounded-[3px]">
         <Link className="logo text-3xl font-bold my-0 mx-[0.2rem] no-underline cursor-pointer text-black" to="/">FoOd CoDe</Link>
-        <div className="navbar m-auto">
-          <nav>
+        <div className="navbar flex justify-between items-center">
+          <nav className="">
             <ul className="list-none">
               <li className={listStyle}>
                 <Link to="/">
@@ -43,13 +43,11 @@ const Header = () => {
                   Cart
                 </Link>
               </li>
+              <li className={`${listStyle}`} onClick={handleButtonClick}>
+                <button className={btnState.class}>{btnState.btnTxt}</button>
+              </li>
             </ul>
           </nav>
-        </div>
-        <div className="m-auto">
-          <div className={`${listStyle} absolute right-[0px] top-[-2px]`} onClick={handleButtonClick}>
-            <button className={btnState.class}>{btnState.btnTxt}</button>
-          </div>
         </div>
       </header>
     </>
